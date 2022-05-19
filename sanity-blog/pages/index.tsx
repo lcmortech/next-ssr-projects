e84e382ed4fa92a77b75717a6c595b7260cf2607
Lauren Morgan <rg326@live.com>
2022-05-18 22:05:52 -0400

    finished hero banner

--- sanity-blog/pages/index.tsx
+++ sanity-blog/pages/index.tsx
@@ -6,20 +6,22 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='max-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-      <div>
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-10'>
         <div className='px-10 space-y-5'>
-          <h1 className='text-6xl max-w-xl font-serif'><span className='underline decoration-black decoration-4'>Medium</span> is a place to write, read, and connect.</h1>
+          <h1 className='text-6xl max-w-xl font-serif'><span className='underline decoration-black decoration-4'>Medium</span>{""} is a place to write, read, and connect.</h1>
           <h2>It's easy and free to post your thinking on any topic and connect with millions of readers.</h2>
         </div>
         <div>
-          <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+          <img
+          className='hidden md:inline-flex h-32 lg:h-full'
+          src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
         </div>
       </div>
     </div>
